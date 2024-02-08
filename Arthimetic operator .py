9759c64aa59052ee6306c7b79542249ae7f06aa3
Jithendiriyan S S <98201677+jithendiriyan@users.noreply.github.com>
2024-02-08 16:32:30 +0530

    Add files via upload

---
+++ Arthimetic operator .py
@@ -0,0 +1,20 @@
+a = 20
+b = 10
+c = a+b #Here C is sum of a and b
+print("Value of c is",c)#c=20+10,So c is 30
+
+F = 20
+D = 10
+G = F-D #Here G is subtraction of F and D
+print("Value of G is",G)#G=20-10, So G is 10
+
+p = 20
+l = 10
+o = p*l #Here o is Multiplication of p and l
+print("Value of o is",o)#0=20*10, So o is 30
+
+w = 20
+q = 10
+s = w / q #Here the w is divided by q.
+print("Value of s is",s)#s=20/10, So o is 2
+
